b2155f3fdc00b618ea46ff1e7dc1678f31f20eab
17Manojsr <2003manojsr@gmail.com>
2024-09-22 20:04:58 +0530

    Initial commit

--- api/bfhl.js
+++ api/bfhl.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow your frontend
+    origin: "http://localhost:3000",
     methods: ["POST", "GET"],
     allowedHeaders: ["Content-Type"],
   })
